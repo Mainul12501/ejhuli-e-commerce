8c742a94ae730a6ebb48a0f7d0e5d4590be1d93b
Mainul12501 <mainul12501@gmail.com>
2022-12-18 23:17:16 +0600

    config files upload

---
+++ config/constants.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'paths' => [
+        'productUpload' => storage_path('app/public/uploads/products/'),
+        'admin_profile' => storage_path('app/public/uploads/profile/admin/'),
+        'customer_profile' => storage_path('app/public/uploads/profile/customer/'),
+        'product_review' => storage_path('app/public/uploads/products-reviews/'),
+        'media' => storage_path('app/public/uploads/media/')
+    ],
+    'shopZEN' =>[
+        'address' => [
+            'street' => '795 Freedom Ave, Suite 6000',
+            'city' => 'Daka',
+            'coutry' => 'Bangladesh',
+            'email' => 'admin@shozen.com',
+            'phone' => '1 (804) 123-9876',
+        ]
+    ],
+    'footer_email' => 'info@ejhuli.com',
+    'footer_mobile' => '+880 1932 974 266',
+    'designed_and_developed_by' => 'VirtualBD',
+    'admin_email' => 'cs.ankitprajapati@gmail.com',
+];
